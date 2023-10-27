a35b6a9395a84ee0a359921667cff3753bb09a29
“CHR” <2692387543@qq.com>
2023-10-27 13:47:12 +0800

    初始化

---
+++ src/test/java/top/chaohaorui/raftkv/RaftkvApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chaohaorui.raftkv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftkvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
